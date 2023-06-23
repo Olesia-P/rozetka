030cb004f4780d5bccecde0b268279285863b792
Olesia-P <120183444+Olesia-P@users.noreply.github.com>
2023-06-23 23:56:10 +0300

    fixed cart link items counter

--- src/components/MobileMenu/CartLink/CartLink.js
+++ src/components/MobileMenu/CartLink/CartLink.js
@@ -1,4 +1,5 @@
 import css from "./CartLink.module.scss";
+import cx from "classnames";
 import { SlBasket } from "react-icons/sl";
 import { useDispatch, useSelector } from "react-redux";
 import { changeIsCartOpen } from "../../../store/modules/commonOpeningSlice";
@@ -15,7 +16,9 @@ export default function CartLink() {
     >
       <SlBasket className={css.logo} />
       <div className={css.link}>Кошик</div>
-      <div className={css.itemsCounter}>{products.length}</div>
+      <div className={cx(css.itemsCounter, products.length > 0 && css.appear)}>
+        {products.length}
+      </div>
     </div>
   );
 }
