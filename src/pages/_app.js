17b5ea308d8494e97496d99d79b916511193938d
Olesia-P <120183444+Olesia-P@users.noreply.github.com>
2023-11-30 17:33:40 +0200

    Added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,17 @@
 import "../styles/globals.scss";
+import Head from "next/head";
 import MobileMenu from "../components/MobileMenu/MobileMenu/MobileMenu.js";
 import Navbar from "../components/Navbar/Navbar.js";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import Cart from "../components/Cart/Cart";
-import IndexPage from "../components/Head/Head.js";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <IndexPage />
+      <Head>
+        <title>Rozetka Homepage Visual Copy</title>
+      </Head>
       <div>
         <Navbar />
         <MobileMenu />
