66bd722f4a46743ad3b06af121053514487591bf
Olesia-P <120183444+Olesia-P@users.noreply.github.com>
2023-12-04 18:29:37 +0200

    Added meta tags

---
+++ src/components/meta/meta.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Meta() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charset="UTF-8" />
+
+      <meta name="title" content="Rozetka Homepage Visual Copy" />
+      <meta
+        name="description"
+        content="This is a visual homepage copy of the famous Ukrainian online shopping website Rozetka.com.ua  "
+      />
+
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://superlative-kleicha-ac1778.netlify.app/"
+      />
+      <meta property="og:title" content="Rozetka Homepage Visual Copy" />
+      <meta
+        property="og:description"
+        content="This is a visual homepage copy of the famous Ukrainian online shopping website Rozetka.com.ua  "
+      />
+      <meta property="og:image" content="rozetka-screenshot-big.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://superlative-kleicha-ac1778.netlify.app/"
+      />
+      <meta property="twitter:title" content="Rozetka Homepage Visual Copy" />
+      <meta
+        property="twitter:description"
+        content="This is a visual homepage copy of the famous Ukrainian online shopping website Rozetka.com.ua  "
+      />
+      <meta property="twitter:image" content="rozetka-screenshot-big.png" />
+    </>
+  );
+}
